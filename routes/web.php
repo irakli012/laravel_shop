b54b05b06046a4eb1cd60aeddf6cf4d7cbbb40e0
Ioseb Matiashvili <i.matiashvili@sweeftdigital.com>
2022-06-03 19:51:16 +0400

    Delete empty spaces

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Homecontroller;
 
 
-
-
-
-
 Route::get('/', [homecontroller::class, 'index']);
 route::get('/about', [homecontroller::class, 'about']);
 route::get('/contact', [homecontroller::class, 'contact']);
@@ -15,21 +11,12 @@ route::get('/contact', [homecontroller::class, 'contact']);
 
 route::get('/store/{category?}/{item?}', function ($category = null, $item = null) {
     if (isset($category)) {
-
-
         if (isset($item)) {
             return "you are viewing the store for {$category} for {$item}";
         }
-
-
-
-
         return 'you are viewing the store for ' . strip_tags($category);
     }
     return 'instruments';
-
-
-
     return "this is    $category ";
 });
 
